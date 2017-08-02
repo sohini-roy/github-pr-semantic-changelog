ee68cb06abce306c9a97fe62e51c7a22c36f10de
sohini-roy <sohinirc45@gmail.com>
2017-08-03 02:12:58 +0530

    recommended bump resolved

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 
 "use strict";
 
+var conventionalGithubReleaser = require('conventional-github-releaser');
+var conventionalRecommendedBump = require('conventional-recommended-bump');
+var validateMessage = require('validate-commit-msg');
 var GitHubApi   = require('github');
 var githubTokenUser = require('github-token-user');
 var github      = new GitHubApi({
@@ -10,7 +13,6 @@ var github      = new GitHubApi({
 
 function createStatus() {
   var argv = require('minimist')(process.argv.slice(2));
-
   var inputString = argv;
   var user_token = inputString._[0];
   var state = inputString._[2];
@@ -23,6 +25,10 @@ function createStatus() {
     'repo' : repository,
     'number' : pullRequestNumber
   }
+  var AUTH = {
+    type: 'oauth',
+    token: user_token
+  };
 
   githubTokenUser(user_token).then(data => {
       // console.log(data);
@@ -46,7 +52,34 @@ function createStatus() {
         if(res){
           for( var i = 0; i<res.data.length; i++){
             console.log(res.data[i].commit.message);
+            var valid = validateMessage(res.data[i].commit.message);
           }
+          conventionalRecommendedBump({
+            preset: 'angular'
+          },
+          function(err, result) {
+            if(err){
+              console.log('conventionalRecommendedBump error');
+              console.log(err);
+            }
+            if(result){
+              console.log("conventionalRecommendedBump response");
+              console.log(result.releaseType);
+              conventionalGithubReleaser(AUTH, {
+                preset: 'angular'
+              },
+              function(err, res){
+                if(err){
+                  console.log("conventionalGithubReleaser error");
+                  console.log(err);
+                }
+                if(res){
+                  console.log("conventionalGithubReleaser response");
+                  console.log(res);
+                }
+              });
+            }
+          });
         }
       }
     );
