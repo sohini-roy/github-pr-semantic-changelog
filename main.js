eab938eb27fa063effd3acf3c0cae371a918f4e5
Gregor Martynus <gregor@martynus.net>
2017-08-26 22:51:40 -0700

    standard (#8)
    * docs(README): update format and some text
    
    * chore(package): standard
    
    * style: standard

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
 #!/usr/bin/env node
 
-"use strict";
+'use strict'
 
-var standardChangelog = require('standard-changelog');
-var gitSemverTags = require('git-semver-tags');
-var conventionalGithubReleaser = require('conventional-github-releaser');
-var conventionalRecommendedBump = require('conventional-recommended-bump');
-var validateMessage = require('validate-commit-msg');
-var GitHubApi   = require('github');
+var standardChangelog = require('standard-changelog')
+var gitSemverTags = require('git-semver-tags')
+var conventionalRecommendedBump = require('conventional-recommended-bump')
+var validateMessage = require('validate-commit-msg')
+var GitHubApi = require('github')
 var github = new GitHubApi({
   version: '3.0.0'
-});
+})
 
-function createStatus() {
-  var argv = require('minimist')(process.argv.slice(2));
+function createStatus () {
+  var argv = require('minimist')(process.argv.slice(2))
   // console.log(argv);
-  var invalidCommits = 0;
-  var inputString = argv;
-  var user_token = inputString._[0];
-  var repo_url = inputString._[1].split('/');
-  var repoOwner = repo_url[3];
-  var repository = repo_url[4];
-  var pullRequestNumber = repo_url[6];
+  var invalidCommits = 0
+  var inputString = argv
+  var userToken = inputString._[0]
+  var repoUrl = inputString._[1].split('/')
+  var repoOwner = repoUrl[3]
+  var repository = repoUrl[4]
+  var pullRequestNumber = repoUrl[6]
   var createStatusInput = {
     'owner': repoOwner,
     'repo': repository,
@@ -31,124 +30,120 @@ function createStatus() {
   }
   var input = {
     'owner': repoOwner,
-    'repo' : repository,
-    'number' : pullRequestNumber
+    'repo': repository,
+    'number': pullRequestNumber
   }
-  var AUTH = {
-    type: 'oauth',
-    token: user_token
-  };
 
-    github.authenticate({
-      type: "token",
-      token: user_token
-    });
+  github.authenticate({
+    type: 'token',
+    token: userToken
+  })
 
-    get_sha();
+  getSha()
 
-    function get_sha(){
-      github.pullRequests.get(
+  function getSha () {
+    github.pullRequests.get(
         input,
-        function(err, res){
-          if(err){
-            console.log("pullRequests get error");
-            console.log(err);
+        function (err, res) {
+          if (err) {
+            console.log('pullRequests get error')
+            console.log(err)
           }
-          if(res){
-            console.log("pullRequests get response");
-            createStatusInput.sha = res.data.head.sha;
-            getCommits();
+          if (res) {
+            console.log('pullRequests get response')
+            createStatusInput.sha = res.data.head.sha
+            getCommits()
           }
         }
-      );
-    }
+      )
+  }
 
-  function getCommits() {
+  function getCommits () {
     github.pullRequests.getCommits(
       input,
-      function(err, res){
+      function (err, res) {
         // console.log(input);
-        if(err){
-          console.log("error");
-          console.log(err);
-          return ;
+        if (err) {
+          console.log('error')
+          console.log(err)
+          return
         }
-        if(res){
-          for( var i = 0; i<res.data.length; i++) {
-            console.log(res.data[i].commit.message);
-            var valid = validateMessage(res.data[i].commit.message);
-            console.log(valid);
+        if (res) {
+          for (var i = 0; i < res.data.length; i++) {
+            console.log(res.data[i].commit.message)
+            var valid = validateMessage(res.data[i].commit.message)
+            console.log(valid)
             // if(valid){
             //   invalidCommits += 1;
             //   console.log("invalid");
             // }
           }
 
-          if(invalidCommits){
-            createStatusInput.description = invalidCommits + '/' + res.data.length + ' commit messages are invalid';
+          if (invalidCommits) {
+            createStatusInput.description = invalidCommits + '/' + res.data.length + ' commit messages are invalid'
             createStatusInput.state = 'error'
             github.repos.createStatus(
               createStatusInput,
-              function(err, res){
-                if(err){
-                  console.log("createStatus error");
-                  console.log(err);
+              function (err, res) {
+                if (err) {
+                  console.log('createStatus error')
+                  console.log(err)
                 }
-                if(res){
-                  console.log("createStatus response");
-                  console.log(res);
+                if (res) {
+                  console.log('createStatus response')
+                  console.log(res)
                 }
               }
-            );
+            )
           }
 
-          if(!invalidCommits){
-            console.log("No invalid commits");
+          if (!invalidCommits) {
+            console.log('No invalid commits')
             createStatusInput.state = 'success'
             github.repos.createStatus(
               createStatusInput,
-              function(err, res){
-                if(err){
-                  console.log("createStatus error");
-                  console.log(err);
+              function (err, res) {
+                if (err) {
+                  console.log('createStatus error')
+                  console.log(err)
                 }
-                if(res){
-                  console.log("createStatus response");
-                  console.log(res);
+                if (res) {
+                  console.log('createStatus response')
+                  console.log(res)
                 }
               }
-            );
+            )
           }
 
           conventionalRecommendedBump({
             preset: 'angular'
           },
-          function(err, result) {
-            if(err){
-              console.log('conventionalRecommendedBump error');
-              console.log(err);
-              return ;
+          function (err, result) {
+            if (err) {
+              console.log('conventionalRecommendedBump error')
+              console.log(err)
+              return
             }
-            if(result){
-              console.log("conventionalRecommendedBump response");
-              console.log(result.releaseType);
-              gitSemverTags(function(err, tags) {
-                if(err){
-                  console.log("git semver error");
-                  console.log(err);
-                  return ;
+            if (result) {
+              console.log('conventionalRecommendedBump response')
+              console.log(result.releaseType)
+              gitSemverTags(function (err, tags) {
+                if (err) {
+                  console.log('git semver error')
+                  console.log(err)
+                  return
                 }
-                if(tags){
-                  console.log(tags);
-                  standardChangelog().pipe(process.stdout);
+                if (tags) {
+                  console.log(tags)
+                  standardChangelog().pipe(process.stdout)
                 }
-              });
+              })
             }
-          });
+          })
         }
       }
-    );
+    )
   }
 }
 
-createStatus();
+createStatus()
